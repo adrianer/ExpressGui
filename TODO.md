708bd2989431a9476be0628698d2771e78f2c4a3
pancaketest <pancakemixerer@hotmail.com>
2016-08-03 19:44:29 +1000

    Add parse_ls_output function

--- TODO.md
+++ TODO.md
@@ -4,7 +4,4 @@
   * Make ui not ugly
 2. Error handling
 3. Add all commands of expressvpn
-4. Create a function called parse_server_list.  
-  Takes an argument that is a string of the output of "expressvpn ls".  
-  Returns a list of servers. Each item being a dictionary that has the keys alias, country, location, recommended.  
 
--- expressvpn.py
+++ expressvpn.py
@@ -21,28 +21,50 @@ def disconnect():
         #disconnected
         return True
 
-def print_servers(server_list):
+def print_servers(server_dict):
     print('--------------------------')
-    for server in server_list:
-        print(server['alias'])
-        print(server['country'])
-        print(server['location'])
-        print(server['recommended'])
+    for country in server_dict['countries']:
+        print("Country = " + country)
+        for location in server_dict[country]:
+            print(location)
 
-def parse_server_list(output):
-    #TODO
+def parse_ls_output(output):
+    """returns a dictionary containing a list of countries and a list of locations
+        ["countries"] for a list of countries """
+    location_list = []
+    server_dict = {}
+    country = None
+    server_dict['countries'] = []
+    output = output.split('\n')            
+    output = output[2:]                     #Remove decoration
+    for server in output:
+        server = server.split('\t')
+        server = filter(None, server)       #Remove blank items
+        if len(server) == 4 or len(server) == 3 and server[2] != "Y":  #check for new country
+            #New country
+            if country != None:                      
+                server_dict['countries'].append(country)        #Add the current country to the server list
+                server_dict[country] = location_list            #Add the list of server locations to the country key
+                location_list = []                              #Start a new list
+                location_list.append(server[2:])                #Add the location to the list
+                country = server[1]                             #Set new current country
+            else:
+                location_list.append(server[2:])                #The first location from output
+                country = server[1]                             #Set current country
+        else:
+            location_list.append(server[1:])                    #Add location
 
-    return server_list
+    return server_dict
 
 def ls():
-    output = subprocess.check_output(["expressvpn", "list"]).split()
-    server_list = parse_server_list(output)
-    return server_list
+    output = subprocess.check_output(["expressvpn", "ls"])
+    location_list = parse_ls_output(output)
+    return location_list
 
 
 if __name__ == "__main__":
     #example of usage
     if status() == False:
         connect()
-    server_list = ls()
-    print_servers(server_list)
+    server_dict = ls()
+    print_servers(server_dict)
