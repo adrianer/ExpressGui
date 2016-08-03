72f65a461aebd9a97e6d8c4e97df5021eeaf04c0
pancaketest <pancakemixerer@hotmail.com>
2016-08-03 21:36:11 +1000

    Fix pep8 styling expressvpn.py

--- expressvpn.py
+++ expressvpn.py
@@ -8,18 +8,21 @@ def status():
         return False
     else:
         return True
-        
+
+
 def connect():
     stream = subprocess.call(["expressvpn", "connect"])
     if stream == 0 or 1:
-        #connected
+        # Connected
         return True
+
 
 def disconnect():
     stream = subprocess.call(["expressvpn", "disconnect"])
     if stream == 0 or 1:
-        #disconnected
+        # Disconnected
         return True
+
 
 def print_servers(server_dict):
     print('--------------------------')
@@ -28,6 +31,7 @@ def print_servers(server_dict):
         for location in server_dict[country]:
             print(location)
 
+
 def parse_ls_output(output):
     """returns a dictionary containing a list of countries and a list of locations
         ["countries"] for a list of countries """
@@ -35,26 +39,35 @@ def parse_ls_output(output):
     server_dict = {}
     country = None
     server_dict['countries'] = []
-    output = output.split('\n')            
-    output = output[2:]                     #Remove decoration
+    output = output.split('\n')
+    output = output[2:]                     # Remove decoration
     for server in output:
         server = server.split('\t')
-        server = filter(None, server)       #Remove blank items
-        if len(server) == 4 or len(server) == 3 and server[2] != "Y":  #check for new country
-            #New country
-            if country != None:                      
-                server_dict['countries'].append(country)        #Add the current country to the server list
-                server_dict[country] = location_list            #Add the list of server locations to the country key
-                location_list = []                              #Start a new list
-                location_list.append(server[2:])                #Add the location to the list
-                country = server[1]                             #Set new current country
+        server = filter(None, server)       # Remove blank items
+        # Check for new country
+        if len(server) == 4 or len(server) == 3 and server[2] != "Y":
+            # New country
+            if country is not None:
+                # Add the current country to the server list
+                server_dict['countries'].append(country)
+                # Add the list of server locations to the country key
+                server_dict[country] = location_list
+                # Start a new list
+                location_list = []
+                # Add the location to the list
+                location_list.append(server[2:])
+                # Set new current country
+                country = server[1]
             else:
-                location_list.append(server[2:])                #The first location from output
-                country = server[1]                             #Set current country
+                # The first location from output
+                location_list.append(server[2:])
+                # Set current country
+                country = server[1]
         else:
-            location_list.append(server[1:])                    #Add location
+            location_list.append(server[1:])      # Add location
 
     return server_dict
+
 
 def ls():
     output = subprocess.check_output(["expressvpn", "ls"])
@@ -63,8 +76,8 @@ def ls():
 
 
 if __name__ == "__main__":
-    #example of usage
-    if status() == False:
+    # Example of usage
+    if status() is False:
         connect()
     server_dict = ls()
     print_servers(server_dict)
