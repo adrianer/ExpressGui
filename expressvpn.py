bbe1f21c3897bb8aee0cc1f2cf419e4711bbffe5
pancaketest <pancakemixerer@hotmail.com>
2016-08-11 21:18:24 +1000

    Add dialog to select server

--- expressvpn.py
+++ expressvpn.py
@@ -1,113 +1,117 @@
 """module to interact with expressvpn"""
 import subprocess
 
+class Expressvpn:
+    connection_status = False
+    current_server = ""
+    current_country = ""
+    servers = {}
 
-def status():
-    """Returns the status of expressvpn, None if disconnected or the location if
-    connected
-    """
-    # Execute expressvpn status and get the output
-    stream = subprocess.check_output(["expressvpn", "status"]).decode('utf-8')
-    if "Not connected" in stream:
-        return None
-    else:
-        # Remove country
-        stream = stream.split(None, 2)
-        # Remove trailing newline
-        stream = stream[2].strip('\n')
-        return stream
+    def __init__(self):
+        self.status()
+        self.ls()
 
+    def status(self):
+        """Returns the status of expressvpn, None if disconnected or the location if
+        connected
+        """
+        # Execute expressvpn status and get the output
+        stream = subprocess.check_output(["expressvpn", "status"]).decode('utf-8')
+        if "Not connected" in stream:
+            self.connection_status = False
+        else:
+            # Remove country
+            stream = stream.split(None, 2)
+            # Remove trailing newline
+            stream = stream[2].strip('\n')
+            self.current_server = stream
+            self.connection_status = True
 
-def connect(location=None):
-    """Connects to the vpn"""
-    if location is None:
-        stream = subprocess.call(["expressvpn", "connect", location])
-    else:
-        stream = subprocess.call(["expressvpn", "connect"])
-    if stream == 0 or 1:
+    def connect(self, location=None):
+        """Connects to the vpn"""
+        if location is not None:
+            stream = subprocess.call(["expressvpn", "connect", location])
+            self.current_server = location
+        else:
+            stream = subprocess.call(["expressvpn", "connect"])
+        if stream == 0 or 1:
         # Connected
-        return True
+            self.connection_status = True
 
 
-def disconnect():
-    """Disconnects from vpn"""
-    stream = subprocess.call(["expressvpn", "disconnect"])
-    if stream == 0 or 1:
+    def disconnect(self):
+        """Disconnects from vpn"""
+        stream = subprocess.call(["expressvpn", "disconnect"])
+        if stream == 0 or 1:
         # Disconnected
-        return True
+            self.connection_status = False
 
-
-def print_servers(server_dict):
-    print('--------------------------')
-    for country in server_dict['countries']:
-        print(country)
+    def print_servers(server_dict):
+        print('--------------------------')
+        for country in server_dict['countries']:
+            print(country)
         for location in server_dict[country]:
             print(location)
 
+    def get_countries_locations(self, output):
+        """Returns the country and a list of locations of that country"""
+        location_list = []
+        country = None
+        output = output.split('\n')             # Remove new line
+        output = output[2:]                     # Remove decoration
 
-def get_countries_locations(output):
-    """Returns the country and a list of locations of that country"""
-    location_list = []
-    country = None
-    output = output.split('\n')             # Remove new line
-    output = output[2:]                     # Remove decoration
-
-    for server in output:
-        server = server.split('\t')
-        server = list(filter(None, server))       # Remove blank items
+        for server in output:
+            server = server.split('\t')
+            server = list(filter(None, server))       # Remove blank items
         # Check for new country
-        if len(server) == 4 or len(server) == 3 and server[2] != "Y":
-            # New country
-            if country is not None:
-                # return the country and its list of locations
-                yield country, location_list
-                # Start a new list
-                location_list = []
-                # Add the location to the list
-                location_list.append(server[2:])
-                # Set new current country
-                country = server[1]
+            if len(server) == 4 or len(server) == 3 and server[2] != "Y":
+                # New country
+                if country is not None:
+                    # return the country and its list of locations
+                    yield country, location_list
+                    # Start a new list
+                    location_list = []
+                    # Add the location to the list
+                    location_list.append(server[2:])
+                    # Set new current country
+                    country = server[1]
+                else:
+                    # The first location from output
+                    location_list.append(server[2:])
+                    # Set current country
+                    country = server[1]
             else:
-                # The first location from output
-                location_list.append(server[2:])
-                # Set current country
-                country = server[1]
-        else:
-            location_list.append(server[1:])      # Add location
+                location_list.append(server[1:])      # Add location
 
-
-def parse_ls_output(output):
-    """Returns a dictionary containing a list of countries and a list of locations
+    def parse_ls_output(self, output):
+        """Returns a dictionary containing a list of countries and a list of locations
         ["countries"] for a list of countries """
-    server_dict = {}
-    server_dict['countries'] = []
-
-    for country, location_list in get_countries_locations(output):
+        server_dict = {}
+        server_dict['countries'] = []
+        for country, location_list in self.get_countries_locations(output):
         # Add the country to country dictionary key
-        server_dict['countries'].append(country)
-        # Add the list of locations to the couontry key
-        server_dict[country] = location_list
+            server_dict['countries'].append(country)
+            # Add the list of locations to the couontry key
+            server_dict[country] = location_list
 
-    return server_dict
+        return server_dict
 
+    def ls(self):
+        """Returns a list of locations"""
+        output = subprocess.check_output(["expressvpn", "ls"]).decode('utf-8')
+        location_list = self.parse_ls_output(output)
+        self.servers = location_list
 
-def ls():
-    """Returns a list of locations"""
-    output = subprocess.check_output(["expressvpn", "ls"]).decode('utf-8')
-    location_list = parse_ls_output(output)
-    return location_list
-
-
-def refresh():
-    """Refreshes the list of locations"""
-    output = subprocess.call(["expressvpn", "refresh"])
-    if output == 0:
-        return True
+    def refresh(self):
+        """Refreshes the list of locations"""
+        output = subprocess.call(["expressvpn", "refresh"])
+        if output == 0:
+            return True
 
 
 if __name__ == "__main__":
-    # Example of usage
+# Example of usage
     if status() is False:
         connect()
-    server_dict = ls()
-    print_servers(server_dict)
+        server_dict = ls()
+        print_servers(server_dict)
--- gui.py
+++ gui.py
@@ -1,125 +1,125 @@
-"""the gui of ExpressvpnGui"""
+"""the gui of expressGui"""
 import os
 import subprocess
 import sys
 import string
 import expressvpn
 import gi
-from gi.repository import Gtk
 gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
 
 class ExpressGui:
-    connected = False
-    server_list = {}
-    current_server = ""
 
     def __init__(self):
+        self.express = expressvpn.Expressvpn()
         # Create containers
+        self.create_main_window()
+        self.create_dialog()
+        # Update the widgets
+        self.update()
+        self.update_servers()
+        # Show the widgets
+        self.window.show_all()
+
+    def create_main_window(self):
         self.window = Gtk.Window()
         self.window.set_border_width(10)
         self.box = Gtk.VBox(False, 0)
         self.window.connect("delete_event", self.delete_event)
         self.window.connect("destroy", self.destroy)
-        # Create comboboxs
-        self.countries_combobox = Gtk.ComboBoxText()
-        self.locations_combobox = Gtk.ComboBoxText()
-        self.statusbar = Gtk.Statusbar()
-        # Create the buttons
+        # Create the widgets
         self.switch = Gtk.Switch()
-        self.refresh_button = Gtk.Button("Refresh")
+        self.location_chooser_button = Gtk.Button('Choose location')
+        self.location_label = Gtk.Label()
         # Connect the signals
-        self.switch.connect("notify::active", self.connect, None)
-        self.refresh_button.connect("clicked", self.refresh, None)
-        self.countries_combobox.connect("changed", self.country_change)
+        self.switch.connect("notify::active", self.switch_toggle, None)
+        self.location_chooser_button.connect("clicked",self.locations_dialog)
         # Pack the widgets
         self.box.pack_start(self.switch, False, False, 0)
-        self.box.pack_start(self.countries_combobox, False, False, 0)
-        self.box.pack_start(self.locations_combobox, False, False, 0)
-        self.box.pack_start(self.refresh_button, False, False, 0)
+        self.box.pack_start(self.location_label, False, False, 0)
+        self.box.pack_start(self.location_chooser_button, False, False, 0)
         # Add them to the window
         self.window.add(self.box)
-        # Show the widgets
-        self.refresh_button.show()
-        self.switch.show()
-        self.countries_combobox.show()
-        self.locations_combobox.show()
-        self.box.show()
-        self.window.show()
-        # Get vpn servers and update status
-        self.update_servers()
-        self.update_status()
 
-    def country_change(self, widget):
-        """Updates the location combobox when country is changed"""
-        self.update_location_box()
+    def create_dialog(self):
+        # Dialog
+        self.dialog = Gtk.Window()
+        # The container
+        box = Gtk.VBox(False, 0)
+        # Add container to dialog
+        self.dialog.add(box)
+        # Create widgets
+        self.refresh_button = Gtk.Button("Refresh")
+        self.countries_combobox = Gtk.ComboBoxText()
+        self.locations_combobox = Gtk.ComboBoxText()
+        self.connect_button = Gtk.Button("Change Server")
+        # Connect signals
+        self.countries_combobox.connect("changed", self.country_change)
+        self.connect_button.connect("clicked", self.dialog_connect) 
+        self.refresh_button.connect("clicked", self.refresh)  
+        # Add widgets to box container
+        box.add(self.countries_combobox)
+        box.add(self.locations_combobox)
+        box.add(self.connect_button)
+        box.add(self.refresh_button)
 
-    def update_country_box(self):
-        """Updates the combobox containging the server countries"""
-        for country in self.server_list['countries']:
-            self.countries_combobox.append_text(country)
-        self.countries_combobox.set_active(0)
+    
+    def locations_dialog(self, widget):
+        """Open dialog to switch server"""
+        self.dialog.show_all()
 
-    def update_location_box(self):
-        """Updates the combobox containing the server locations"""
-        self.locations_combobox.get_model().clear()
-        country = self.countries_combobox.get_active_text()
-        for location in self.server_list[country]:
-            self.locations_combobox.append_text(location[0])
-        self.locations_combobox.set_active(0)
+    def dialog_connect(self, widget):
+        location = self.locations_combobox.get_active_text()
+        status = self.express.connection_status
+        if status is False:
+            self.express.connect(location)
+        elif status is True and self.express.current_server != location:
+            self.express.disconnect()
+            self.express.connect(location)
 
-    def update_servers(self):
-        """Gets the servers from expressvpn then updates the comboboxs"""
-        self.get_servers()
-        self.update_country_box()
-        self.update_location_box()
+        self.update()
 
-    def get_servers(self):
-        """Gets a list of servers from expressvpn"""
-        self.server_list = expressvpn.ls()
-
-    def update_status(self):
-        """Gets expressvpn connection status"""
-        status = expressvpn.status()
-        if status is None:
-            self.current_server = status
-            self.connected = True
-        else:
-            self.connected = False
-        self.update_switch()
+    def update_location_label(self):
+        """Updates the server location label"""
+        self.location_label.set_text(self.express.current_server)
 
     def update_switch(self):
         """Updates the state of the switch"""
-        if self.connected is False:
+        if self.express.connection_status is False:
             self.switch.set_state(False)
         else:
             self.switch.set_state(True)
 
-    def connect(self, switch, gparam, location=None):
+    def update(self):
+        self.update_location_label()
+        self.update_switch()
+
+
+    def switch_toggle(self, switch, gparam, test):
         """ Callback function for the active signal of the switch
             Connects if active disconnects if inactive
         """
         location = self.locations_combobox.get_active_text()
-        if switch.get_active():
-            # switch active so connect if not already connected
-            if location != self.current_server:
-                expressvpn.disconnect()
-                expressvpn.connect(location)
+        if self.switch.get_active():
+            # switch active so connect if not already connection_status
+            if location is not None and self.express.connection_status is not True:
+                self.express.disconnect()
+                self.express.connect(location)
+            else:
+                self.express.connect()
         else:
             # switch set to inactive so disconnect
-            expressvpn.disconnect()
+            self.express.disconnect()
+
+        self.update()
+
 
     def disconnect(self, widget, data=None):
-        if expressvpn.disconnect() is True:
-            self.connected = False
+        if self.express.disconnect() is True:
+            self.connection_status = False
         else:
-            self.connected = True
-        self.update_status()
-
-    def refresh(self, widget, data=None):
-        """Refreshes the list of servers"""
-        expressvpn.refresh()
-        self.update_servers()
+            self.connection_status = True
 
     def delete_event(self, widget, event, data=None):
         print("Exiting")
@@ -130,6 +130,41 @@ class ExpressGui:
 
     def main(self):
         Gtk.main()
+
+    def refresh(self, widget, data=None):
+        """Refreshes the list of servers"""
+        self.express.refresh()
+        self.update_servers()
+
+    def update_country_box(self):
+        """Updates the combobox containging the server countries"""
+        for country in self.express.servers['countries']:
+            self.countries_combobox.append_text(country)
+        self.countries_combobox.set_active(0)
+
+    def update_location_box(self):
+        """Updates the combobox containing the server locations"""
+        self.locations_combobox.get_model().clear()
+        country = self.countries_combobox.get_active_text()
+        for location in self.express.servers[country]:
+            self.locations_combobox.append_text(location[0])
+        self.locations_combobox.set_active(0)
+
+    def country_change(self, widget):
+        """Updates the location combobox when country is changed"""
+        self.update_location_box()
+
+    def get_servers(self):
+        """Gets a list of servers from express"""
+        self.express.ls()
+
+    def update_servers(self):
+        """Gets the servers from express then updates the comboboxs"""
+        self.get_servers()
+        self.update_country_box()
+        self.update_location_box()
+
+
 
 if __name__ == "__main__":
     express = ExpressGui()
