9094b32f5cf1e674884dcd194826e3a6bcce72d2
pancaketest <pancakemixerer@hotmail.com>
2016-08-22 00:52:25 +1000

    Screenshot script now works for location dialog

---
+++ screenshots/screenshot.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append("..")
+from Window import Window
+import subprocess
+from threading import Thread
+from time import sleep
+
+expressgui = Window()
+mythread = Thread(target=expressgui.main)
+mythread.start()
+sleep(1)
+subprocess.call(["gnome-screenshot", "-w", "-f", "window.png"])
+expressgui.emit("destroy")
+expressgui = Window()
+expressgui.location_chooser_button.emit("clicked")
+mythread = Thread(target=expressgui.main)
+mythread.start()
+sleep(1)
+subprocess.call(["gnome-screenshot", "-w", "-f", "locationchooser.png"])
+expressgui.emit("destroy")
+
